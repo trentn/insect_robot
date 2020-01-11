ca20a54918cc14091b75b7f9191375a8a5e75867
Trent Novelly <trentnov@gmail.com>
2020-01-11 21:53:39 +0000

    using older version

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.61.tar.gz
-tar bcm2835-1.61.tar.gz
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
+tar xzvf bcm2835-1.60.tar.gz
 
-cd bcm2835-1.61/
+cd bcm2835-1.60/
 ./configure
 make
+sudo make check
 sudo make install
 cd ../
 
