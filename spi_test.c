2ace314a7d555f9101f9d3ab8687c70576eef450
Trent Novelly <trentnov@gmail.com>
2020-01-12 22:39:38 +0000

    comment update

--- spi_test.c
+++ spi_test.c
@@ -1,21 +1,3 @@
-// spi.c
-//
-// Example program for bcm2835 library
-// Shows how to interface with SPI to transfer a byte to and from an SPI device
-//
-// After installing bcm2835, you can build this 
-// with something like:
-// gcc -o spi spi.c -l bcm2835
-// sudo ./spi
-//
-// Or you can test it before installing with:
-// gcc -o spi -I ../../src ../../src/bcm2835.c spi.c
-// sudo ./spi
-//
-// Author: Mike McCauley
-// Copyright (C) 2012 Mike McCauley
-// $Id: RF22.h,v 1.21 2012/05/30 01:51:25 mikem Exp $
-
 #include "servo_controller.h"
 #include <unistd.h>
 
