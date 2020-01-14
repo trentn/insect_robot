9160037123c6d8a7b863a7429e28ac871aa253ce
Trent Novelly <trentnov@gmail.com>
2020-01-14 01:09:18 +0000

    updating dependecies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
-tar xzvf bcm2835-1.60.tar.gz
+BCMLIBRARY="bcm2835-1.60"
+APT_PACKAGES="libncurses5 libncurses5-dev"
 
-cd bcm2835-1.60/
+echo "Installing $BCMLIBRARY..."
+if [ ! -d "$BCMLIBRARY" ]; then
+wget http://www.airspayce.com/mikem/bcm2835/$BCMLIBRARY.tar.gz
+tar xzvf $BCMLIBRARY.tar.gz
+
+cd $BCMLIBRARY
 ./configure
 make
 sudo make check
@@ -11,3 +16,11 @@ sudo make install
 cd ../
 
 echo "Installed bcm2835 library"
+else
+    echo "$BCMLIBRARY is already installed"
+    
+fi
+
+echo ""
+echo "Installing apt packages $APT_PACKAGES..."
+sudo apt-get install -y $APT_PACKAGES
