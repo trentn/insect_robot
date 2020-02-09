06f9ab5383d6ff13fd12571841947ffcabbfc409
Trent Novelly <trentnov@gmail.com>
2020-02-08 21:28:11 -0500

    setup for ability to crosscompile

---
+++ ToolChain.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+
+set(CMAKE_C_COMPILER $ENV{HOME}/x-tools/armv6-rpi-linux-gnueabi/bin/armv6-rpi-linux-gnueabi-gcc)
+
+set(CMAKE_FIND_ROOT_PATH $ENV{HOME}/x-tools/armv6-rpi-linux-gnueabi)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,13 +3,31 @@
 BCMLIBRARY="bcm2835-1.60"
 APT_PACKAGES="cmake libncurses5 libncurses5-dev"
 
+CROSS_COMPILE=""
+BCM_LOCATION=""
+
+while (( "$#" )); do
+    case "$1" in
+        -x|--cross-compile)
+            CROSS_COMPILE="--host $2"
+            BCM_LOCATION="--prefix $PWD/extras"
+            shift 2
+            ;;
+        *)
+            echo "Error: unsupported arguments"
+            exit 1
+            ;;
+    esac
+done
+
+
 echo "Installing $BCMLIBRARY..."
 if [ ! -d "$BCMLIBRARY" ]; then
 wget http://www.airspayce.com/mikem/bcm2835/$BCMLIBRARY.tar.gz
 tar xzvf $BCMLIBRARY.tar.gz
 
 cd $BCMLIBRARY
-./configure
+./configure $CROSS_COMPILE $BCM_LOCATION
 make
 sudo make check
 sudo make install
